9b38d8dc82024e9f619f3874cfb6e0d6ac1b5655
Karol Uracz <karol_ur@interia.pl>
2020-06-16 13:25:35 +0200

    homeAction added bags count

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -3,22 +3,31 @@ package pl.coderslab.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller
 public class HomeController {
 
     private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
 
     @RequestMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutions", institutionRepository.findAll());
+        Integer bagsSum = donationRepository.bagsSum().orElse(0);
+        model.addAttribute("bags", bagsSum);
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -1,9 +1,16 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.entity.Donation;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DonationRepository extends JpaRepository<Donation, Long> {
+    List<Donation> findAll();
+    @Query("SELECT SUM (quantity) FROM Donation ")
+    Optional<Integer> bagsSum();
 }
