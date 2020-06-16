4ffcce968c4db3b4f87b153e1c6d2507790b72a5
Karol Uracz <karol_ur@interia.pl>
2020-06-16 10:31:05 +0200

    initial commit

---
+++ src/main/java/pl/coderslab/charity/entity/Category.java
@@ -0,0 +1,14 @@
+package pl.coderslab.charity.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String name;
+}
