aedcf4a6703128d3df033c30d837850bb98add86
Karol Uracz <karol_ur@interia.pl>
2020-06-18 15:49:01 +0200

    login and 403 views, starting security config

--- src/main/java/pl/coderslab/charity/config/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/config/SecurityConfig.java
@@ -1,10 +1,21 @@
 package pl.coderslab.charity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .and().formLogin().loginPage("/login")
+                .and().logout().logoutSuccessUrl("/")
+                .permitAll();
+    }
 }
